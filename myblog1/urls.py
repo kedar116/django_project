ececfdcd3654ec68b731af55c3a9cccae2e704b3
Kedarnath <venkatkedarnath@gmail.com>
2019-10-10 22:18:52 +0530

    First Commit

---
+++ myblog1/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import ( 
+	PostListView,
+	PostDetailView,
+	PostCreateView,
+	PostUpdateView,
+	PostDeleteView,
+	UserPostListView
+)
+
+from . import views
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/new', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+]
